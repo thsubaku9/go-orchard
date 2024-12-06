9d1432bf9eefd90683c6c2fc49d5e04fcaa19f7c
kaustubh <kaustubhkj@gmail.com>
2024-12-07 00:05:56 +0530

    worker skelly

--- task/task.go
+++ task/task.go
@@ -1,6 +1,10 @@
 package task
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type State int
 
@@ -23,7 +27,22 @@ func (s State) String() string {
 }
 
 type Task struct {
-	ID    uuid.UUID
-	Name  string
-	State State
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  map[int]struct{}
+	PortBindings  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
 }
--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,31 @@
 package worker
+
+import (
+	"fmt"
+	"orchard/task"
+	"sync/atomic"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount atomic.Int32
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
+}
