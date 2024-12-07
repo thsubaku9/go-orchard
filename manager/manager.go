cf18c4a8ffbef0a943c76e7e26ef05d3b895fd15
kaustubh <kaustubhkj@gmail.com>
2024-12-07 13:15:37 +0530

    scheduler + node skelly

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,28 @@
 package manager
+
+import (
+	"fmt"
+	"orchard/task"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]uuid.UUID
+	EventDbmap    map[uuid.UUID]string
+	Workers       []string
+	WorkerTaskMap map[string][]*task.Task
+	TaskWorkerMap map[string][]*task.Task
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker")
+}
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks")
+}
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
+}
