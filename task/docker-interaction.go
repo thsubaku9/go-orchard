7c495ad0d1a86de4343720d0fe4d3cec69f8da0a
kaustubh <kaustubhkj@gmail.com>
2024-12-08 12:12:18 +0530

    docker run method wip

--- task/docker-interaction.go
+++ task/docker-interaction.go
@@ -1,13 +1,49 @@
 package task
 
 import (
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"context"
+	"io"
+	"log"
+	"os"
+
+	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
 )
 
 type Docker struct {
 	Client *client.Client
 	Config Config
+}
+
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, types.ImagePullOptions{})
+
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	io.Copy(os.Stdout, reader)
+
+	return DockerResult{} // todok
+}
+
+func (d *Docker) Inspect(containerId string) DockerInspectResponse {
+	dc, err := client.NewEnvClient()
+	if err != nil {
+		return DockerInspectResponse{Error: err}
+	}
+
+	ctx := context.Background()
+
+	res, err := dc.ContainerInspect(ctx, containerId)
+
+	if err != nil {
+		return DockerInspectResponse{Error: err}
+	}
+
+	return DockerInspectResponse{Container: &res}
 }
 
 type DockerResult struct {
