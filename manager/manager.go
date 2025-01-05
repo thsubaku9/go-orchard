172f82cb382a94887b74647a26619a188e2efca8
kaustubh <kaustubhkj@gmail.com>
2025-01-05 22:58:46 +0530

    SendWork in progress

--- manager/manager.go
+++ manager/manager.go
@@ -1,7 +1,9 @@
 package manager
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"orchard/task"
 
 	"github.com/golang-collections/collections/queue"
@@ -10,19 +12,46 @@ import (
 
 type Manager struct {
 	Pending       queue.Queue
-	TaskDb        map[string][]uuid.UUID
-	EventDbmap    map[uuid.UUID]string
+	TaskDb        map[uuid.UUID]*task.Task
+	EventDb       map[uuid.UUID]*task.TaskEvent
 	Workers       []string
-	WorkerTaskMap map[string][]*task.Task
-	TaskWorkerMap map[string][]*task.Task
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+	LastWorker    int
 }
 
-func (m *Manager) SelectWorker() {
-	fmt.Println("I will select an appropriate worker")
+func (m *Manager) SelectWorker() string {
+	workerId := m.Workers[m.LastWorker]
+
+	m.LastWorker += 1
+	m.LastWorker = m.LastWorker % len(m.Workers)
+	return workerId
 }
+
+func (m *Manager) SendWork() {
+	if m.Pending.Len() == 0 {
+		return
+	}
+
+	workerId := m.SelectWorker()
+	te := m.Pending.Dequeue().(task.TaskEvent)
+	log.Printf("Pulled %v off pending queue\n", te.Task)
+	m.EventDb[te.ID] = &te
+
+	m.WorkerTaskMap[workerId] = append(m.WorkerTaskMap[workerId], te.Task.ID)
+	m.TaskWorkerMap[te.Task.ID] = workerId
+	te.Task.State = task.Scheduled
+
+	m.TaskDb[te.Task.ID] = &te.Task
+
+	data, err := json.Marshal(te)
+	if err != nil {
+		log.Printf("Unable to marshal taskevent object: %v.\n", te)
+	}
+
+	//todo -> sending of the data to worker
+}
+
 func (m *Manager) UpdateTasks() {
 	fmt.Println("I will update tasks")
-}
-func (m *Manager) SendWork() {
-	fmt.Println("I will send work to workers")
 }
