c7f95b78ff2478897be15cee8bc1cf4c4ea24898
kaustubh <kaustubhkj@gmail.com>
2025-01-15 10:35:42 +0530

    inital roundrobin stub

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,29 @@
 package scheduler
 
+import (
+	"orchard/node"
+	"orchard/task"
+)
+
 type Scheduler interface {
-	SelectCandidateNodes()
-	ScoreNodes()
-	PickNode()
+	SelectCandidateNodes(t task.Task, allNodes []*node.Node) []*node.Node
+	ScoreNodes(t task.Task, candidateNodes []*node.Node) map[string]float64
+	PickNode(scores map[string]float64, candidateNodes []*node.Node) *node.Node
+}
+
+type RoundRobin struct {
+	Name       string
+	LastWorker int
+}
+
+func (rr *RoundRobin) SelectCandidateNodes(t task.Task, allNodes []*node.Node) []*node.Node {
+	return allNodes
+}
+
+func (rr *RoundRobin) ScoreNodes(t task.Task, candidateNodes []*node.Node) map[string]float64 {
+	panic("not implemented") // TODO: Implement
+}
+
+func (rr *RoundRobin) PickNode(scores map[string]float64, candidateNodes []*node.Node) *node.Node {
+	panic("not implemented") // TODO: Implement
 }
